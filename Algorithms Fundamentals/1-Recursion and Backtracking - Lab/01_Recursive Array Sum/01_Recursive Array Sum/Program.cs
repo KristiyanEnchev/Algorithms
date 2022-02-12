0b9a4a065de24e4ec762ff4583f499eb90f245c5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-02-12 20:35:43 +0200

    Adding new exercises From Algorithms Course - Recursion

---
+++ Algorithms Fundamentals/1-Recursion and Backtracking - Lab/01_Recursive Array Sum/01_Recursive Array Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(SumRecursivly(numbers, 0));
+        }
+
+        private static int SumRecursivly(int[] numbers, int index)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + SumRecursivly(numbers, index + 1);
+        }
+    }
+}
