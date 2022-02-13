5db0931e2c707aa2c26bb151a3747a40ce37eb1f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-02-13 23:05:58 +0200

    Exercise Algorithms

---
+++ Algorithms Fundamentals/1-Recursion and Backtracking - Lab/02_Recursive Factorial/02_Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02_Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateFacturiel(n));
+        }
+
+        private static int CalculateFacturiel(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalculateFacturiel(n - 1);
+        }
+    }
+}
