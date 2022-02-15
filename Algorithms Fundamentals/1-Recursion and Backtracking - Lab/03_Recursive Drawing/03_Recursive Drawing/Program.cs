2cd0633201511d0cdfd32855d482d24a54f739e8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-02-15 21:38:23 +0200

    More Algorithms Exercises

---
+++ Algorithms Fundamentals/1-Recursion and Backtracking - Lab/03_Recursive Drawing/03_Recursive Drawing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03_Recursive_Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int row)
+        {
+            if (row == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', row));
+
+            DrawFigure(row - 1);
+
+            Console.WriteLine(new string('#', row));
+
+        }
+    }
+}
