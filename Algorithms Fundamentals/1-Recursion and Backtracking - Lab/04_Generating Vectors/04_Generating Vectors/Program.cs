c893a56501e5f703e74d0677ea65b236d4b8e1f2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-02-22 20:26:42 +0200

    Adding more Algorithms Exercises

---
+++ Algorithms Fundamentals/1-Recursion and Backtracking - Lab/04_Generating Vectors/04_Generating Vectors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04_Generating_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var arr = new int[n];
+
+            GenerateVectors(arr, 0);
+        }
+
+        private static void GenerateVectors(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[index] = i;
+
+                GenerateVectors(arr, index + 1);
+            }
+        }
+    }
+}
