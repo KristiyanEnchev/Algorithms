6bc0aa520a51e49d20d877e242abb5f203658f52
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-02-22 20:27:38 +0200

    Adding more Algorithms Exercises

---
+++ Algorithms Fundamentals/1-Recursion and Backtracking - Lab/05_Generating Combinations/05_Generating Combinations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Generating_Combinations
+{
+    class Program
+    {
+        static void Main()
+        {
+            var set = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var k = int.Parse(Console.ReadLine());
+
+            var vector = new int[k];
+
+            Gen(set, vector, 0, 0);
+        }
+
+        private static void Gen(int[] set, int[] vector, int index, int border)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (var i = border; i < set.Length; i++)
+                {
+                    vector[index] = set[i];
+
+                    Gen(set, vector, index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
