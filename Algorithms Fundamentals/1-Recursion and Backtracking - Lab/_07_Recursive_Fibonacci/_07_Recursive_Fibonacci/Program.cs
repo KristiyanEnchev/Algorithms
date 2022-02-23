0f6a8f475e9812826d2d003c73e6302c45db2298
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-02-23 21:13:58 +0200

    Some Algorithms exegesis

---
+++ Algorithms Fundamentals/1-Recursion and Backtracking - Lab/_07_Recursive_Fibonacci/_07_Recursive_Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07_Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+        private static int GetFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            }
+        }
+    }
+}
