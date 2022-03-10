6e84c2078c4f06896849555c81258e026befc1ce
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-03-10 22:34:10 +0200

    Adding more algorithms exercises

---
+++ Algorithms Fundamentals/3-Recursion and Combinatorial Problems - Exercise/01_Reverse_Array/01_Reverse_Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            ReverseArray(arr, 0, arr.Length - 1);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void ReverseArray(int[] arr, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            ReverseArray(arr, ++start, --end);
+        }
+    }
+}
