4b08b0a28ab90c203be986c6d7065e6349cfea81
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-03-11 23:15:04 +0200

    More Exercises Algorithms

---
+++ Algorithms Fundamentals/3-Recursion and Combinatorial Problems - Exercise/02_Nested_Loops/02_Nested_Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02_Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var arr = new int[number];
+
+            PrintCombinations(arr, 0);
+        }
+
+        private static void PrintCombinations(int[] arr, int start)
+        {
+            if (start == arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                arr[start] = i;
+                PrintCombinations(arr, start + 1);
+            }
+        }
+    }
+}
