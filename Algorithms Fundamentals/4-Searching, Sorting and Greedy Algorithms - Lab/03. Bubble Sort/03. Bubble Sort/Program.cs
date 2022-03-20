c61ce85ea2d6f9af3b2626df7a4997a1a1d702c4
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-03-20 23:42:18 +0200

    Exercises Algorithms

---
+++ Algorithms Fundamentals/4-Searching, Sorting and Greedy Algorithms - Lab/03. Bubble Sort/03. Bubble Sort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            BubbleSort(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void BubbleSort(int[] arr)
+        {
+            var isSorted = false;
+            var sortedCount = 0;
+
+            while (!isSorted)
+            {
+                isSorted = true;
+
+                for (int right = 1; right < arr.Length - sortedCount; right++)
+                {
+                    var left = right - 1;
+
+                    if (arr[left] > arr[right])
+                    {
+                        Swap(arr, left, right);
+                        isSorted = false;
+                    }
+                }
+
+                sortedCount += 1;
+            }
+        }
+
+        private static void Swap(int[] numbers, int start, int end)
+        {
+            var temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+        }
+    }
+}
