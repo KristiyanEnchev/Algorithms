ed264c80b7ee84d557d34ed86338711ef73f3c0b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2022-03-27 23:18:36 +0300

    Exercises on algorithms

---
+++ Algorithms Fundamentals/4-Searching, Sorting and Greedy Algorithms - Lab/07. Sum of Coins/07. Sum of Coins/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Sum_of_Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = new Queue<int>(Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .OrderByDescending(x => x));
+
+            var target = int.Parse(Console.ReadLine());
+            var usedCoins = new Dictionary<int, int>();
+            var totalCoins = 0;
+
+            while (target > 0 && coins.Count > 0)
+            {
+                var currentCoin = coins.Dequeue();
+                var count = target / currentCoin;
+
+                if (count == 0)
+                {
+                    continue;
+                }
+
+                usedCoins[currentCoin] = count;
+                totalCoins += count;
+
+                target %= currentCoin;
+            }
+
+            if (target == 0)
+            {
+                Console.WriteLine($"Number of coins to take: {totalCoins}");
+
+                foreach (var coin in usedCoins)
+                {
+                    Console.WriteLine($"{coin.Value} coin(s) with value {coin.Key}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
